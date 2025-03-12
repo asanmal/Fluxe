c4538741f7454a090535b52d53f399f6fb93abe5
asanmal <asanmal0312@g.educaand.es>
2025-03-12 10:10:37 +0100

    Initial commit

---
+++ app/src/main/java/com/tfg/PantallaDeCarga.java
@@ -0,0 +1,31 @@
+package com.tfg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PantallaDeCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pantalla_de_carga);
+
+        //Esto demora la pantalla de carga
+        final int duracion = 2500;
+
+        new Handler().postDelayed(() -> {
+            //Se ejecuta pasado el tiempo de duracion que le hemos puesto
+            Intent intent = new Intent(PantallaDeCarga.this, MainActivity.class);
+            startActivity(intent);
+            //Nos dirije de esta actividad, al mainACtivity
+        }, duracion);
+    }
+}
