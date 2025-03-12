2a85e4e9a1828cfb77e9df993603bc493edb540d
asanmal <asanmal0312@g.educaand.es>
2025-03-12 13:47:06 +0100

    Pantalla de registro, inicio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.android.application")
     id("com.google.gms.google-services")
 }
 
@@ -46,4 +45,16 @@ dependencies {
     //Firebase 12-03-2025
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    //Animacion, diseno, bbdd, gestion, recycler, cardview 12-03-2025
+    implementation("com.airbnb.android:lottie:3.0.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
+    //implementation("com.squared.picasso:picaso:2.71828.21.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+
 }
--- app/src/main/java/com/tfg/MainActivity.java
+++ app/src/main/java/com/tfg/MainActivity.java
@@ -1,6 +1,9 @@
 package com.tfg;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn, registroBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginBtn = findViewById(R.id.loginBtn);
+        registroBtn = findViewById(R.id.registroBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
         });
+
+        registroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
+        });
+
+
     }
 }
